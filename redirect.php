5ccd1718497b6e796cdc9cd356ee2114472a58c5
MSI0072 <63404067+MSI0072@users.noreply.github.com>
2021-02-11 07:32:51 +0700

    Update redirect.php

--- redirect.php
+++ redirect.php
@@ -1,18 +1,29 @@
 <?php
-if (isset($_GET['name']) && isset($_GET['email']) && isset($_GET['tlp']) && isset($_GET['alamat']) && isset($_GET['date']) && isset($_GET['pesan']) && isset($_GET['tipe']) && isset($_GET['ug']))
+if (isset($_POST['jenis']) && isset($_POST['produk']) && isset($_POST['ug']) && isset($_POST['name']) && isset($_POST['email']) && isset($_POST['tlp']) && isset($_POST['alamat']) && isset($_POST['date']) && isset($_POST['note']))
 {
-
-    $name = $_GET['name'];
-    $email = $_GET['email'];
-    $tlp = $_GET['tlp'];
-    $alamat = $_GET['alamat'];
-    $date = $_GET['date'];
-    $pesan = $_GET['pesan'];
-    $tipe = $_GET['tipe'];
-    $text = '&text=➸ *Nama :* ' . $name . '%0A➸ *Email :* ' . $email . '%0A➸ *No Tlp :* ' . $tlp . '%0A➸ *Alamat :* ' . $alamat . '%0A➸ *Keperluan :* Saya ingin membuat janji temu pada ' . $date . '%0A➸ *Tipe :* ' . $tipe . '%0A➸ *Pesan :* ' . $pesan;
-    $url_wa = $_GET['ug'];;
-    header('Location:' . $url_wa . $text);
-    exit;
-
+    $jenis = $_POST['jenis'];
+    $produk = $_POST['produk'];
+    $ug = $_POST['ug'];
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $tlp = $_POST['tlp'];
+    $alamat = $_POST['alamat'];
+    $date = $_POST['date'];
+    $note = $_POST['note'];
+    if ($jenis == '1')
+    {
+        $text = '&text=Assalamualaikum, dibawah ini adalah identitas saya :%0A%0A➸ *Nama :* ' . $name . '%0A➸ *No tlp :* ' . $tlp . '%0A➸ *Email :* ' . $email . '%0A➸ *Alamat :* ' . $alamat . '%0A%0AKeperluan saya adalah ingin membuat janji temu pada ' . $date . ', untuk membahas ' . $produk . ' terima kasih.%0A%0A*Note :* ' . $note;
+        $url_wa = $_POST['ug'];;
+        header('Location:' . $url_wa . $text);
+        exit;
+    }
+    else
+    {
+        $text = '&text=Assalamualaikum, dibawah ini adalah identitas saya :%0A%0A➸ *Nama :* ' . $name . '%0A➸ *No tlp :* ' . $tlp . '%0A➸ *Email :* ' . $email . '%0A➸ *Alamat :* ' . $alamat . '%0A%0AKeperluan saya adalah ingin berkonsultasi tentang ' . $produk . ' terima kasih.%0A%0A*Note :* ' . $note;
+        $url_wa = $_POST['ug'];;
+        header('Location:' . $url_wa . $text);
+        exit();
+    }
+  
 };
 ?>
